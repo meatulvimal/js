eec686630c52c664eb726b6a907fe47e988b010e
meatulvimal <110231890+meatulvimal@users.noreply.github.com>
2023-10-28 14:13:57 +0000

    conversion_datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "hkbaih"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+// 23 => 23
+//
+
+let isLoggedIn = "khba"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
